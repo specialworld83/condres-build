40d75c10798e24a9ee708251516d64348ba1cc7f
Team Codelinsoft <specialworld83@gmail.com>
2019-06-03 08:13:34 +0200

    [upiso] Announce iso apricity relives and new update iso. Remove package octopi and condres-setting-manager

--- condres-gnome/build.sh
+++ condres-gnome/build.sh
@@ -254,7 +254,7 @@ make_prepare() {
 
 # Build ISO
 make_iso() {
-    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "condres_os-19.05-${iso_name}.iso"
+    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "condres_os-19.06-${iso_name}.iso"
 }
 
 
